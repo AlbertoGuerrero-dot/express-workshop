c5dfdcb990ee9f23bad88aee0aed7bb70fd7b4d9
Luis Alberto <delacruz.guerrero.luisalberto@gmail.com>
2020-10-26 23:58:08 -0600

    delete and patch

--- Routes/pokemon.js
+++ Routes/pokemon.js
@@ -21,6 +21,23 @@ pokemon.post('/', async (req, res, next)=>{
     
 });
 
+pokemon.delete("/:id([0-9]{1,3})", async (req, res, next) => {
+    const query = `DELETE FROM pokemon WHERE pok_id=${req.params.id}`;
+    const rows = await db.query(query);
+    
+    if (rows.affectedRows == 1) {
+        return res.status(200).json({code: 200, message: "Pokemon borrado correctamente"});
+    }
+    return res.status(404).json({ code: 404, message: "Pokemon no encontrado" });
+});
+
+pokemon.put("/:id([0-9]{1,3})", async (req, res, next) => {
+    const { pok_name, pok_height, pok_weight, pok_base_experience} = req.body;
+    let query = `UPDATE pokemon SET pok_name=,pok_height=,`;
+    query += `pok_weight=[value-4],pok_base_experience= WHERE pok_id=${req.params.id};`;
+
+});
+
 pokemon.get('/', async (req, res, next) => {
     const pkmn = await db.query("SELECT * FROM pokemon");
     return res.status(200).json({code: 200, message: pkmn});
@@ -28,7 +45,7 @@ pokemon.get('/', async (req, res, next) => {
 
 pokemon.get('/:id([0-9]{1,3})', async (req, res, next) => {
     const id = req.params.id;
-    if (id >= 1 && id <= 722) {
+    if (id >= 1 && id <= 723) {
         const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_id="+id+";");
         return res.status(200).json({code: 200, message: pkmn});
     } 
