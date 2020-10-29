d2b82f763f431ffab8888a053c9f91374a821b23
Luis Alberto <delacruz.guerrero.luisalberto@gmail.com>
2020-10-28 20:45:59 -0600

    user added

--- Routes/pokemon.js
+++ Routes/pokemon.js
@@ -32,10 +32,35 @@ pokemon.delete("/:id([0-9]{1,3})", async (req, res, next) => {
 });
 
 pokemon.put("/:id([0-9]{1,3})", async (req, res, next) => {
-    const { pok_name, pok_height, pok_weight, pok_base_experience} = req.body;
-    let query = `UPDATE pokemon SET pok_name=,pok_height=,`;
-    query += `pok_weight=[value-4],pok_base_experience= WHERE pok_id=${req.params.id};`;
 
+    const { pok_name, pok_height, pok_weight, pok_base_experience} = req.body;
+    if (pok_name && pok_height && pok_weight && pok_base_experience) {
+        let query = `UPDATE pokemon SET pok_name='${pok_name}',pok_height=${pok_height},`;
+        query += `pok_weight=${pok_weight},pok_base_experience=${pok_base_experience} WHERE pok_id=${req.params.id};`;
+
+    const rows = await db.query(query);
+    console.log(rows);
+    
+    if(rows.affectedRows == 1) {
+    return res.status(200).json({code :200, message: "pokemon actualizado correctamente"});
+    }
+    return res.status(500).json({ code: 500, message: "Ocurrio un error"});
+    }
+    return res.status(500).json({code:500, message: "Campos incompletos"});
+
+});
+
+pokemon.patch("/:id([0-9]{1,3})", async (req, res, next) => {
+
+    if(req.body.pok_name) {
+    let query = `UPDATE pokemon SET pok_name='${req.body.pok_name}' WHERE pok_id=${req.params.id}`;
+    const rows = await db.query(query);
+    if(rows.affectedRows == 1) {
+    return res.status(200).json({code :200, message: "pokemon actualizado correctamente"});
+    }
+    return res.status(500).json({code:500, message: "Ocurrió un error"});;
+    }
+    return res.status(500).json({code: 500, message:"Campos incompletos" });
 });
 
 pokemon.get('/', async (req, res, next) => {
@@ -45,7 +70,7 @@ pokemon.get('/', async (req, res, next) => {
 
 pokemon.get('/:id([0-9]{1,3})', async (req, res, next) => {
     const id = req.params.id;
-    if (id >= 1 && id <= 723) {
+    if (id >= 1 && id <= 722) {
         const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_id="+id+";");
         return res.status(200).json({code: 200, message: pkmn});
     } 
